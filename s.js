ea815dcb4e80fb9dea9cd092edf86767ae65fe28
aneeshaugustinehub <aneeshaugustine911@gmail.com>
2023-09-29 13:04:00 +0530

    animation Added

--- s.js
+++ s.js
@@ -1,4 +1,17 @@
-function myFunction() {
+/* function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
-  }
+  } */
+const observer = new IntersectionObserver((entries) =>{
+  entries.forEach((entry) =>{
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }else {
+      entry.target.classList.remove('show');
+    }
+  })
+})
+
+  const hidddenElement = document.querySelectorAll('.hidden');
+  hidddenElement.forEach((el) => observer.observe(el))
