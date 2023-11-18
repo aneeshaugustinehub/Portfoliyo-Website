6d6c855505004f62df6a9714363eb1838cea5193
aneeshaugustinehub <aneeshaugustine911@gmail.com>
2023-11-18 13:01:14 +0530

    dynamic copyright year

--- s.js
+++ s.js
@@ -2,22 +2,20 @@
     var element = document.body;
     element.classList.toggle("dark-mode");
   } */
-const observer = new IntersectionObserver((entries) =>{
-  entries.forEach((entry) =>{
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
     console.log(entry);
     if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    }else {
-      entry.target.classList.remove('show');
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
     }
-  })
-})
+  });
+});
 
-  const hidddenElement = document.querySelectorAll('.hidden');
-  hidddenElement.forEach((el) => observer.observe(el))
+const hidddenElement = document.querySelectorAll(".hidden");
+hidddenElement.forEach((el) => observer.observe(el));
 
-  let date = new Date().getFullYear();
-
-  let copy = '<p>&copy; '+ date +' aneesh augustine</p>'
-
-  document.getElementById('year').innerHTML = copy
+let date = new Date().getFullYear();
+let copy = "<p>&copy; " + date + " aneesh augustine</p>";
+document.getElementById("year").innerHTML = copy;
