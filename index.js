755aa3b765f2cc80365af5f0f6e9e3bde72e3b0e
aneeshaugustinehub <aneeshaugustine911@gmail.com>
2026-05-26 15:11:09 +0530

    refactor: update HTML and JavaScript for consistency and readability

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 let date = new Date().getFullYear();
-let copy = "<p>&copy; " + date + " aneesh augustine</p>";
-document.getElementById("year").innerHTML = copy;
+let copy = '<p>&copy; ' + date + ' aneesh augustine</p>';
+document.getElementById('year').innerHTML = copy;
 
 const toggle = document.getElementById('theme-toggle');
 
@@ -15,6 +15,15 @@ toggle.addEventListener('click', () => {
 
 // Load saved preference on page load
 if (localStorage.getItem('theme') === 'dark' ||
-  (!localStorage.getItem('theme') && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    (!localStorage.getItem('theme') &&
+     window.matchMedia('(prefers-color-scheme: dark)').matches)) {
   document.documentElement.classList.add('dark');
 }
+
+
+const menuToggle = document.getElementById('menu-toggle');
+const mobileMenu = document.getElementById('mobile-menu');
+menuToggle.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+  mobileMenu.classList.toggle('flex');
+});
